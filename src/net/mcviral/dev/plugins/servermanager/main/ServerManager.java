eb240a8ca1ae14f2785201a25b124d0750b42bc5
Matt <Matt@Matt-Desktop-PC>
2015-03-19 04:14:17 +0000

    Initial commit

---
+++ src/net/mcviral/dev/plugins/servermanager/main/ServerManager.java
@@ -0,0 +1,15 @@
+package net.mcviral.dev.plugins.servermanager.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerManager extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
